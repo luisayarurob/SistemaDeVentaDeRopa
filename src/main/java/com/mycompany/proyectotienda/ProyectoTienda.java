9f1b89c8c4069856d4c6b0ede7407261a5512152
Aleja <maria.ramirez41@udea.edu.co>
2026-04-08 20:43:35 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/proyectotienda/ProyectoTienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectotienda;
+
+/**
+ *
+ * @author aleja
+ */
+public class ProyectoTienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
